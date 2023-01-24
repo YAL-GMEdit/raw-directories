3dcc53133f95e02c5db97917391ac3924b00e6fc
YellowAfterlife <yellowafterlife@hotmail.com>
2023-01-24 14:00:49 +0200

    Fixed the Project Properties input joining multiple paths incorrectly; you can now reference files instead of directories

--- raw-directories.js
+++ raw-directories.js
@@ -3,6 +3,7 @@
 	let RawLoader = $gmedit["raw.RawLoader"];
 	let Preferences = $gmedit["ui.Preferences"];
 	let ProjectProperties = $gmedit["ui.project.ProjectProperties"];
+	let FileSystem = Electron_FS;
 	function onOpen(e) {
 		let pj = e.project;
 		switch (pj.version.config.loadingMode) {
@@ -10,13 +11,20 @@
 			default: return;
 		}
 		if (pj.path == null) return;
+		
 		let arr = pj.properties.rawDirectories || ["."];
 		for (let path of arr) (function addRawDirectory(path) {
 			if (path != "." && !pj.existsSync(path)) {
-				console.error("Can't add `"+path+"` - it does not exist");
+				console.error(`Can't add ${path} - it does not exist`);
 				return;
 			}
+			let fullPath = pj.fullPath(path);
 			let name = path != "." ? path : "Raw view";
+			if (!FileSystem.statSync(fullPath).isDirectory()) {
+				let item = TreeView.makeAssetItem(path, path, fullPath, "file");
+				TreeView.element.appendChild(item);
+				return;
+			}
 			let dir = TreeView.makeAssetDir(name, path, "file");
 			let isReady = false;
 			dir.treeHeader.addEventListener("click", function ensureRawDirectory(_) {
@@ -37,7 +45,7 @@
 				let arr = pj.properties.rawDirectories || ["."];
 				let el = Preferences.addInput(gr,
 					"Directory list (separated with `|`)",
-					arr.join("."),
+					arr.join("|"),
 				function updateDirList(s) {
 					pj.properties.rawDirectories = s ? s.split("|") : [];
 					ProjectProperties.save(pj, pj.properties);
